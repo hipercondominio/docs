a996eaf5912f12d9f20b646760f5b5250ac63969
hipercondominio <hipercondominio@users.noreply.github.com>
2018-02-22 23:28:22 -0300

    Transferência de arquivos.

---
+++ Preview/AutoCondominio/AutoCondominio.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutoCondominio.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
